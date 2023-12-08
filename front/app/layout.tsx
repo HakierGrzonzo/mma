38c321a478f3db3c0fc560e5031eec5dbe6de914
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2023-12-08 23:29:03 +0100

    change favicon

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -24,7 +24,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e3606c" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#e3606c" />
+      </head>
       <body>
         <main>{children}</main>
         <footer>
