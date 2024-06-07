abf07f2ce1550a2225e85a7cb7126e7c4f567268
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2024-06-07 12:04:14 +0200

    Add README and update infra

---
+++ README.md
@@ -0,0 +1,49 @@
+# MoringMark Archive
+
+https://moringmark.grzegorzkoperwas.site/
+
+An archive of **The Owl House** comics made by [u/makmark](https://www.reddit.com/user/makmark) and posted to the subreddit.
+
+Made cause I wanted to read all the parts of [Grom Factor](https://moringmark.grzegorzkoperwas.site/comic/Grom%20Factor/) in order.
+
+## Features:
+
+- Individual comics are merged into series
+- A sortable list of comics
+- OCR in alt text
+- Automatic update via scheduled jobs
+
+It is just a static website built from json files, what more do you expect from it.
+
+## Technical stuff
+
+I used this project to learn AWS.
+
+The website is hosted by an `s3`, proxied by `cloudfront`.
+
+Two ECS tasks (built by github actions) process reddit posts and store them in `s3`, simple stuff.
+
+
+## How to develop
+
+### Requirements:
+
+- A unixlike operating system
+- `node` and `npm`
+- `python 3.12` and `poetry`
+
+### Instructions:
+
+1. Clone the repo
+2. Acquire reddit API secret and export it into env variable `SECRET`.
+3. Go to `./scraper` and launch `poetry install` to install scraper dependencies
+4. Launch `poetry run -m src` to gather the data from reddit
+    - You can pass an env parameter `COMIC_LIMIT` to limit the amount of comics
+5. Go to `./front` and launch `npm install`
+6. Run `npm run dev` to see the development version in your browser
+7. Run `npm run build` to see the final result
+
+There is also a docker-compose file, you could add some volumes to make it work
+locally or add references to two s3 buckets in `BUCKET` and `DESTINATION` env 
+variables.
+
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  applicationARN = "arn:aws:resource-groups:us-east-1:767397670578:group/mma/0epa3txbvzptdclxtbl6yiw89s"
+  cert_arn = "arn:aws:acm:us-east-1:767397670578:certificate/e9263c10-140d-4b45-a27d-2dd10269d145"
+  one_hour_cache_id = "67394ec7-583a-41c0-8fc7-3f62f7d59947"
+  caching_optimized = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+}
+
 resource "aws_s3_bucket" "mma-images" {
   bucket = "mma-images"
   tags = {
     Name        = "mma-images"
     Environment = "Dev"
+    awsApplication = local.applicationARN
   }
 }
 
@@ -15,6 +23,7 @@ resource "aws_s3_bucket" "mma-web" {
   tags = {
     Name        = "mma-web"
     Environment = "Dev"
+    awsApplication = local.applicationARN
   }
 }
 
@@ -76,6 +85,7 @@ resource "aws_ecr_repository" "mma-scraper" {
   image_tag_mutability = "MUTABLE"
   tags = {
     Environment = "Dev"
+    awsApplication = local.applicationARN
   }
 }
 resource "aws_ecr_repository" "mma-front" {
@@ -83,23 +93,23 @@ resource "aws_ecr_repository" "mma-front" {
   image_tag_mutability = "MUTABLE"
   tags = {
     Environment = "Dev"
+    awsApplication = local.applicationARN
   }
-}
-
-locals {
-  cert_arn = "arn:aws:acm:us-east-1:767397670578:certificate/e9263c10-140d-4b45-a27d-2dd10269d145"
-  one_hour_cache_id = "67394ec7-583a-41c0-8fc7-3f62f7d59947"
-  caching_optimized = "658327ea-f89d-4fab-a63d-7e88639e58f6"
 }
 
 resource "aws_cloudfront_distribution" "mma-images" {
   origin {
-    domain_name = aws_s3_bucket.mma-images.bucket_domain_name
+    domain_name = aws_s3_bucket.mma-images.website_endpoint
     origin_id   = "mma-images"
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "index.html"
 
   aliases = ["img.moringmark.grzegorzkoperwas.site"]
 
@@ -125,16 +135,24 @@ resource "aws_cloudfront_distribution" "mma-images" {
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method = "sni-only"
   }
+  tags = {
+    awsApplication = local.applicationARN
+  }
 }
 
 resource "aws_cloudfront_distribution" "mma-web" {
   origin {
-    domain_name = aws_s3_bucket.mma-web.bucket_domain_name
+    domain_name = aws_s3_bucket.mma-web.website_endpoint
     origin_id   = "mma-web"
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "index.html"
 
   aliases = ["dev.moringmark.grzegorzkoperwas.site"]
 
@@ -153,6 +171,65 @@ resource "aws_cloudfront_distribution" "mma-web" {
       restriction_type = "none"
     }
   }
+  tags = {
+    awsApplication = local.applicationARN
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+  }
+
+
+  viewer_certificate {
+    acm_certificate_arn = local.cert_arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+  }
+}
+resource "aws_cloudfront_distribution" "mma-prod" {
+  origin {
+    domain_name = aws_s3_bucket.mma-web.website_endpoint
+    origin_id   = "mma-web-prod"
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  aliases = ["moringmark.grzegorzkoperwas.site"]
+
+  default_cache_behavior {
+    cached_methods = ["GET", "HEAD"]
+    compress = true
+    viewer_protocol_policy = "redirect-to-https"
+    # Using the one-hour policy ID:
+    cache_policy_id  = local.one_hour_cache_id
+    allowed_methods  = ["GET", "HEAD"]
+    target_origin_id = "mma-web-prod"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  tags = {
+    awsApplication = local.applicationARN
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+  }
 
 
   viewer_certificate {
@@ -164,6 +241,9 @@ resource "aws_cloudfront_distribution" "mma-web" {
 
 resource "aws_route53_zone" "mma" {
   name = "moringmark.grzegorzkoperwas.site"
+  tags = {
+    awsApplication = local.applicationARN
+  }
 }
 
 import {
@@ -190,6 +270,18 @@ resource "aws_route53_record" "web" {
   type    = each.key
   alias {
     name = aws_cloudfront_distribution.mma-web.domain_name
+    zone_id = aws_cloudfront_distribution.mma-web.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "web-prod" {
+  zone_id = aws_route53_zone.mma.zone_id
+  name    = ""
+  for_each = toset(["A", "AAAA"])
+  type    = each.key
+  alias {
+    name = aws_cloudfront_distribution.mma-prod.domain_name
     zone_id = aws_cloudfront_distribution.mma-web.hosted_zone_id
     evaluate_target_health = false
   }
