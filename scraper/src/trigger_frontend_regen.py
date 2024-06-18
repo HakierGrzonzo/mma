e2e9c9d280add97a1733166fffa3fdc9ea13dfb3
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2024-06-18 14:44:48 +0200

    Move ARN for trigger_frontend_regen into env

--- scraper/src/trigger_frontend_regen.py
+++ scraper/src/trigger_frontend_regen.py
@@ -1,25 +1,27 @@
 from logging import getLogger
+from os import environ
 import boto3
 
 logger = getLogger(__name__)
 
+CLUSTER_ARN = environ.get("CLUSTER_ARN")
+TASK_DEFINITION_ARN = environ.get("TASK_DEFINITION_ARN")
+
 
 def trigger_frontend_regen():
+    if CLUSTER_ARN is None or TASK_DEFINITION_ARN is None:
+        raise Exception("Cluster and task not specified")
     client = boto3.client("ecs")
     logger.info("Triggering Frontend task")
     client.run_task(
-        cluster="arn:aws:ecs:us-east-1:767397670578:cluster/mma-test",
+        cluster=CLUSTER_ARN,
         count=1,
         launchType="FARGATE",
-        taskDefinition="arn:aws:ecs:us-east-1:767397670578:task-definition/front-mma",
+        taskDefinition=TASK_DEFINITION_ARN,
         networkConfiguration={
             "awsvpcConfiguration": {
-                "subnets": [
-                    "subnet-00c5cd223fe861889",
-                ],
-                "securityGroups": [
-                    "sg-0d0cabbabacb7842f",
-                ],
+                "subnets": [],
+                "securityGroups": [],
                 "assignPublicIp": "ENABLED",
             }
         },
