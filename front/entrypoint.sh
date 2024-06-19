4694874bccd56f1be1a96a2a3e43a3c5442aed55
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2024-06-19 10:16:54 +0200

    Fix issues

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run build && cd ./out && aws s3 sync . "s3://$DESTINATION/" --size-only --delete --acl public-read
+npm run build && cd ./out && aws s3 sync . "s3://$DESTINATION/" --acl public-read
