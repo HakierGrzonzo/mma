6127a9484b19658d8abf7f099fb0a9d416a746c1
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2025-04-14 21:26:30 +0200

    Prettify search

--- front/components/search.tsx
+++ front/components/search.tsx
@@ -1,14 +1,36 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
+import classes from "./search.module.css";
 import { useSqlQuery } from "./SqliteContext";
+import { usePathname, useSearchParams } from "next/navigation";
+import { getSubmissionLinks } from "@/utils";
 
 export function Search() {
-  const [query, setQuery] = useState("grom");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const query = searchParams.get("query") || "";
+  const setQuery = (value: string) => {
+    const encoded = encodeURIComponent(value);
+    history.replaceState(null, "", `${pathname}?query=${encoded}`);
+  };
   const pattern = `%${query}%`;
-  const result = useSqlQuery<{ id: string; title: string }>(
+  const result = useSqlQuery<{
+    id: string;
+    title: string;
+    ocr: string;
+    titleMatch: boolean;
+    ocrMatch: boolean;
+    comicTitle: string;
+    isMultipart: boolean;
+  }>(
     `SELECT
         comic_series.id AS id,
-        comic_series.title AS title
+        comic_series.title AS title,
+        comic_series.title LIKE ? as titleMatch,
+        comic.title as comicTitle,
+        (SELECT COUNT(*) > 1 FROM comic WHERE series = comic_series.id) as isMultipart,
+        image.ocr as ocr,
+        image.ocr LIKE ? as ocrMatch
       FROM
         comic_series
       JOIN
@@ -18,28 +40,40 @@ export function Search() {
         image
         ON image.comic = comic.id
       WHERE
-        comic_series.title LIKE ?
+        titleMatch
         OR 
-        image.ocr LIKE ?
+        ocrMatch
       GROUP BY comic_series.id
       ;
     `,
     [pattern, pattern],
   );
   return (
-    <div>
+    <section className={classes.container}>
+      <h1>Search</h1>
       <input
+        className={classes.search}
         value={query}
         type="search"
+        placeholder="Type to search for comics"
         onChange={(e) => setQuery(e.target.value)}
       />
       <ul>
-        {result.map((r) => (
-          <li key={r.id}>
-            <a href={`/comic/${encodeURIComponent(r.id)}`}>{r.title}</a>
-          </li>
-        ))}
+        {result.map((r) => {
+          const { comicLink } = getSubmissionLinks(
+            `/comic/${encodeURIComponent(r.id)}`,
+            r.comicTitle,
+          );
+          return (
+            <li key={r.id}>
+              <Link title={r.ocrMatch ? r.ocr : undefined} href={comicLink}>
+                {r.title}{" "}
+                {r.ocrMatch && r.isMultipart ? `(${r.comicTitle})` : null}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
-    </div>
+    </section>
   );
 }
