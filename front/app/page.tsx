6b6bc08849a0c86ac77523c591c71e30ca6ebe5f
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2025-10-13 14:04:31 +0200

    Revert "Add Kog cover image every 3rd day"
    This reverts commit 3445eed12301bb046662aff188b144cede262279.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -42,7 +42,7 @@ export default async function Home() {
           .
         </p>
         <p>
-          Now with <em>Knights of Guinevere</em> comics!
+          Now with <em>Knights of Guinevere</em> comics?! (Work in progress)
         </p>
       </CoverImage>
       <section>
